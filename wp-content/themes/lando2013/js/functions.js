da3b125f90cde6d042ec20ecc2bf729ae8e7c82e
Brandon Hundt <brandonhundt@gmail.com>
2017-08-28 10:38:06 -0500

    changed sidebar to use flexbox. Also updated home-mid section to contain additional items

--- wp-content/themes/lando2013/js/functions.js
+++ wp-content/themes/lando2013/js/functions.js
@@ -9,22 +9,6 @@
 	    _window = $( window );
 
 	/**
-	 * Adds a top margin to the footer if the sidebar widget area is higher
-	 * than the rest of the page, to help the footer always visually clear
-	 * the sidebar.
-	 */
-	$( function() {
-		if ( body.is( '.sidebar' ) ) {
-			var sidebar   = $( '#secondary .widget-area' ),
-			    secondary = ( 0 == sidebar.length ) ? -40 : sidebar.height(),
-			    margin    = $( '#tertiary .widget-area' ).height() - $( '#content' ).height() - secondary;
-
-			if ( margin > 0 && _window.innerWidth() > 999 )
-				$( '#colophon' ).css( 'margin-top', margin + 'px' );
-		}
-	} );
-
-	/**
 	 * Enables menu toggle for small screens.
 	 */
 	( function() {
